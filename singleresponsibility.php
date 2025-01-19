ab5a53a9efb7ff6671078112358cbb5814a9d964
Mosin S. Sayyed <mosin143243@gmail.com>
2025-01-19 15:04:54 +0530

    Single responsibility Principle

---
+++ singleresponsibility.php
@@ -0,0 +1,75 @@
+<?php
+
+class circle {
+    private $radius;
+    
+    public function setredious($redious)
+    {
+        $this->radius = $redious;
+    }
+
+    public function getArea()
+    {
+        return pi() * pow($this->radius, 2);
+    }
+
+}
+
+class square 
+{
+ private $length;
+
+    public function setLength($length)
+    {
+        $this->length = $length;
+    }
+
+    public function getArea()
+    {
+        return pow($this->length, 2);
+    }
+
+
+}
+
+class AreaCalculator {
+    
+    private $shapes;
+
+    public function __construct(...$shapes)
+    {
+        $this->shapes = $shapes;
+    }
+
+    public function output()
+    {
+
+        foreach($this->shapes as $shape)
+        {
+            if($shape instanceof circle){
+                echo "Area of Circle :". $shape->getArea();
+            }else if($shape instanceof square){
+                echo "Area of square :". $shape->getArea();
+            }
+            echo "<br>";
+        }
+    }
+
+
+    
+}
+
+
+$currentCircle = new circle();
+$square = new square();
+
+$currentCircle->setredious(5);
+ 
+$square->setLength(5);
+
+$calculateAre= new AreaCalculator($currentCircle, $square);
+$calculateAre->output();    
+
+
+
+?>
