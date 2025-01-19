2ad56e375c1d958406534dee173d6204503e974d
Mosin S. Sayyed <mosin143243@gmail.com>
2025-01-19 17:02:22 +0530

    liskov substitution principle

---
+++ interfacesegrigation.php
@@ -0,0 +1,3 @@
+<?php  
+//interface segregation principle
+//A client should never be forced to implement an interface that it doesn't use or clients shouldn't be forced to depend on methods they do not use.
