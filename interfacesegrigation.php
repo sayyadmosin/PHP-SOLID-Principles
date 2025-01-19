a59b6e5de9083f8b505f1c8334756e1823547cee
Mosin S. Sayyed <mosin143243@gmail.com>
2025-01-19 17:35:03 +0530

    interface segregation principle

--- interfacesegrigation.php
+++ interfacesegrigation.php
@@ -1,3 +1,68 @@
 <?php  
 //interface segregation principle
 //A client should never be forced to implement an interface that it doesn't use or clients shouldn't be forced to depend on methods they do not use.
+
+interface canFly
+{
+    public function fly();
+}
+
+interface canSwim
+{
+    public function swim();
+}
+interface canRun
+{
+    public function run();
+}
+
+class Bird implements canFly
+{
+    public function fly()
+    {
+        echo "Bird is flying";
+    }
+}
+
+class Fish implements canSwim
+{
+    public function swim()
+    {
+        echo "Fish is swimming";
+    }
+}
+
+class Dog implements canRun
+{
+    public function run()
+    {
+        echo "Dog is running";
+    }
+}
+
+class Duck implements canFly, canSwim
+{
+    public function fly()
+    {
+        echo "Duck is flying";
+    }
+
+    public function swim()
+    {
+        echo "Duck is swimming";
+    }
+}
+
+
+$bird = new Bird();
+$bird->fly();
+echo "<br>";
+
+$duck = new Duck();
+$duck->fly();
+echo "<br>";
+$duck->swim();
+echo "<br>";
+$dog = new Dog();
+$dog->run();
+echo "<br>";
